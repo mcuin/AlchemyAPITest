9aaeba63f0a9e4a682a7954baf2c44a9d14df039
Mykal Cuin <mykcuin@gmail.com>
2015-08-05 10:37:19 -0600

    Added a new page to display data. Made main/index the route.

--- AlchemyTest/app/controllers/main_controller.rb
+++ AlchemyTest/app/controllers/main_controller.rb
@@ -2,10 +2,11 @@ class MainController < ApplicationController
 
   def index
     alchemyapi = :keyword
+    twitter_size = :twitter_Size
   end
 
   def search
-    Main.twitterSearch()
+    Main.twitterSearch(:keyword, :twitter_Size)
   end
 
 end
---
+++ AlchemyTest/app/models/stackoverflow.rb
@@ -0,0 +1,4 @@
+require './alchemyapi'
+require 'ruby-stackoverflow'
+
+
